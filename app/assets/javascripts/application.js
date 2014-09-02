8c1a760725e0ad55e3a301202fb67f57aa956710
flyptkarsh <ptkarsh@gmail.com>
2014-09-02 10:24:00 -0400

    set up resources call for got yaas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,52 +18,60 @@
 //= require_tree ./templates
 //= require_tree .
 
-
+// creates the entire angular application 
 var app = angular.module('GotyaaApp', ['ngResource', 'templates'])
+  // configures $resource for the factories 
   .config(['$resourceProvider', function ($resourceProvider) {}])
+  // factory for making ajaxy angular calls to the message database
+  .factory('GotYaas', ['$resource', function($resource) {
+    return $resource('http://localhost:3000/got_yaas', null,
+       {});
+  }])
+  // controls adding the message and creating a new gotyaa 
+  .controller('GotyaaController', ['$scope', 'GotYaas', function($scope, GotYaas) {
+    var gotYaasPromise = GotYaas.query();
+    console.log(gotYaasPromise); 
+    $scope.messages = [];
+
+    // adds the content to the message 
+    $scope.submit = function() {
+      if ($scope.smsContent) {
+        $scope.messages.push(this.smsContent);
+        $scope.smsContent = '';
+      }
+    };
+
+    // deletes a gotyaa before it is sent 
+    $scope.removeMessage = function(message) {
+      console.log('clicked removeMessage'); 
+      var i = $scope.messages.indexOf(message); 
+      $scope.messages.splice(i, 1);
+    }; 
+  }])
+  // factory to make angular ajaxy requests to database 
   .factory('Recipients', ['$resource', function($resource) {
+   // makes http request to the server 
    return $resource('http://localhost:3000/got_yaas/@/recipients', null,
        {});
-   }])
-  // controls adding the message and creating a new gotyaa 
-  .controller('GotyaaController', ['$scope', function($scope) {
-    
-  $scope.messages = [];
-
-  // adds the content to the message 
-  $scope.submit = function() {
-    if ($scope.smsContent) {
-      $scope.messages.push(this.smsContent);
-      $scope.smsContent = '';
-    }
-  };
-
-  // deletes a gotyaa before it is sent 
-  $scope.removeMessage = function(message) {
-    console.log('clicked removeMessage'); 
-    var i = $scope.messages.indexOf(message); 
-    $scope.messages.splice(i, 1);
-  }; 
-
   }]) 
   // controls adding the recipients to a new gotyaa 
   .controller('RecipientsController', ['$scope', function($scope) { 
-  
-  $scope.recipients = [];
-
-  $scope.submitRecipient = function() {
-  console.log('RecipientsController submit clicked'); 
-  if ($scope.recipient) {
-    $scope.recipients.push(this.recipient);
-    $scope.recipient = ''; 
-  }
-  };
+    
+    $scope.recipients = [];
+    //submits a new recipient in the browser 
+    $scope.submitRecipient = function() {
+    console.log('RecipientsController submit clicked'); 
+    if ($scope.recipient) {
+      $scope.recipients.push(this.recipient);
+      $scope.recipient = ''; 
+    }
+    };
   }]) 
   // makes a get request to the back-end for message status, displays status
   .controller('SentGotyaaController', ['$scope', 'Recipients', function($scope, Recipients) { 
+    //returns all recipients in a promise 
     var recipientsPromise = Recipients.query(); 
-    var recipientsData = recipientsPromise.data; 
-    console.log(recipientsData); 
+    console.log(recipientsPromise); 
   }
 ]); 
 
