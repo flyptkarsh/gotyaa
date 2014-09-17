01b85b8338437ae373ce1b80d94e2cf21b9dea0d
flyptkarsh <ptkarsh@gmail.com>
2014-09-17 18:04:55 -0400

    blah

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,9 +8,6 @@ class TwilioController < ApplicationController
     account_sid = ENV["TWILIO_SID"]
     auth_token = ENV["TWILIO_TOKEN"]
 
-    params_body.to_s! 
-
-    params_body << " please reply by texting back 'ok' to this number"
     @client = Twilio::REST::Client.new account_sid, auth_token
      # send the message to each recipient
     @client.account.messages.create({
